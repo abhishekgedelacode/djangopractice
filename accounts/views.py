ac2e1c6613c1db95b90c0300c6434be14a3b043c
abhishekgedelacode <gabhishekji143@gmail.com>
2021-01-09 19:32:50 +0530

    create view and create template are created and fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student
+from .forms import Stdform
 
 # Create your views here.
 
@@ -13,3 +14,14 @@ def display(request):
     context = {}
     context['dataset'] = Student.objects.all()
     return render(request, 'accounts/display.html', context)
+
+
+def create_view(request):
+    context = {}
+    form = Stdform(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+    context['form'] = form
+    return render(request, 'accounts/create_view.html', context)
