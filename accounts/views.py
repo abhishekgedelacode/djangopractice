01627dcbf915b25a8a3a8a7f0290963fe109c1fe
abhishekgedelacode <gabhishekji143@gmail.com>
2021-01-09 19:15:32 +0530

    addition of views in views py file def display to render all the students in our db

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("<h1>hello world <hr/> smart code </h1>")
+
+
+def display(request):
+    context = {}
+    context['dataset'] = Student.objects.all()
+    return render(request, 'accounts/display.html', context)
